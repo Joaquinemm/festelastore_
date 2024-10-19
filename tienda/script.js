a585b2c8442d0c7d03a11c82e733c5fd1f5b4fe3
Joaquin Mendez Melo <164065335+Joaquinemm@users.noreply.github.com>
2024-10-19 19:59:41 -0400

    Update script.js

--- tienda/script.js
+++ tienda/script.js
@@ -29,34 +29,34 @@ function iniciar() {
 }
 
 function restaurar(){
-    var imagen = document.getElementById('sueter_1').src = "/productos/sueter.webp";
-    var imagen_2 = document.getElementById('sueter_2').src = "/productos/sueter_2.webp";
-    var imagen_3 = document.getElementById('sueter_3').src = "/productos/sueter_3.webp";
-    var imagen_4 = document.getElementById('sueter_4').src = "/productos/sueter_4.jpg";
-    var imagen_5 = document.getElementById('sueter_5').src = "/productos/sueter_5.jpg";
-    var imagen_6 = document.getElementById('sueter_6').src = "/productos/sueter_6.jpg";
+    var imagen = document.getElementById('sueter_1').src = "../productos/sueter.webp";
+    var imagen_2 = document.getElementById('sueter_2').src = "../productos/sueter_2.webp";
+    var imagen_3 = document.getElementById('sueter_3').src = "../productos/sueter_3.webp";
+    var imagen_4 = document.getElementById('sueter_4').src = "../productos/sueter_4.jpg";
+    var imagen_5 = document.getElementById('sueter_5').src = "../productos/sueter_5.jpg";
+    var imagen_6 = document.getElementById('sueter_6').src = "../productos/sueter_6.jpg";
 }
 
 function peligro() {
-    var imagen = document.getElementById('sueter_1').src = "/tallas/sueter.webp";
+    var imagen = document.getElementById('sueter_1').src = "../tallas/sueter.webp";
 }
 
 function peligro2() {
-    var imagen_2 = document.getElementById('sueter_2').src = "/tallas/sueter_2.jpg";
+    var imagen_2 = document.getElementById('sueter_2').src = "../tallas/sueter_2.jpg";
 }
 
 function peligro3() {
-    var imagen_3 = document.getElementById('sueter_3').src = "/tallas/sueter_3.webp";
+    var imagen_3 = document.getElementById('sueter_3').src = "../tallas/sueter_3.webp";
 }
 
 function peligro4() {
-    var imagen_4 = document.getElementById('sueter_4').src = "/tallas/sueter_4.jpg";
+    var imagen_4 = document.getElementById('sueter_4').src = "../tallas/sueter_4.jpg";
 }
 
 function peligro5() {
-    var imagen_5 = document.getElementById('sueter_5').src = "/tallas/sueter_5.jpg";
+    var imagen_5 = document.getElementById('sueter_5').src = "../tallas/sueter_5.jpg";
 }
 
 function peligro6() {
-    var imagen_6 = document.getElementById('sueter_6').src = "/tallas/sueter_6.jpg";
+    var imagen_6 = document.getElementById('sueter_6').src = "../tallas/sueter_6.jpg";
 }
